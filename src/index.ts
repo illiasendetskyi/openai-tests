270157eef4c8994a4ffe599d7e7a2d256985a8fc
Illia Sendetskyi <illiasendetskyi@gmail.com>
2023-03-20 10:48:27 +0200

    feat: add createCompletion command

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
 import { Configuration, OpenAIApi } from 'openai';
 import { ORG as organization, SK as apiKey } from '../secrets';
 import type { ConfigurationParameters } from 'openai';
-import { createReadStream } from 'fs';
+// import { createReadStream } from 'fs';
 
 async function bootstrap(conf: ConfigurationParameters): Promise<void> {
   const configuration = new Configuration(conf);
   const openai = new OpenAIApi(configuration);
 
-  const image = createReadStream('untitled.png');
-  const response = await openai.createImageEdit(
-    image as any,
-    'put the helmet on the head of the guy',
-    undefined,
-    2,
-    '512x512'
-  );
-  console.log(response.status);
-  console.log(response.data.data);
+  // const image = createReadStream('untitled.png');
+  // const response = await openai.createImageEdit(
+  //   image as any,
+  //   'put the helmet on the head of the guy',
+  //   undefined,
+  //   2,
+  //   '512x512'
+  // );
+  // console.log(response.status);
+  // console.log(response.data.data);
+
+  const prompt =
+    'create a set of 5 random questions with 3 incorrect and one correct answer options about features and the universe of For Honor Ubisoft game';
+
+  console.log(`sending prompt: ${prompt}`);
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt,
+    temperature: 0.7,
+    max_tokens: 1024,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  response.data.choices.forEach((choice) => {
+    if (typeof choice.text === 'string') {
+      process.stdout.write(choice.text);
+    }
+  });
+  process.stdout.write('\n');
+  console.log(JSON.stringify(response.data, null, 2));
 }
 
 void bootstrap({
